e5d5a1c369f7a89f7d88ea95637098a87c1217f3
Kuba663 <kuba307@poczta.onet.pl>
2026-07-05 15:54:13 +0200

    Cleanup Imports

--- src/main/java/com/cleanroommc/physics/PhysicsCommon.java
+++ src/main/java/com/cleanroommc/physics/PhysicsCommon.java
@@ -1,23 +1,11 @@
 package com.cleanroommc.physics;
 
-import it.unimi.dsi.fastutil.PriorityQueue;
-import it.unimi.dsi.fastutil.objects.ObjectHeapPriorityQueue;
 import net.minecraftforge.fml.common.InjectedModContainer;
 import net.minecraftforge.fml.common.ModContainer;
 import net.minecraftforge.fml.common.asm.FMLSanityChecker;
 import com.cleanroommc.physics.config.PhysicsConfig;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.jspecify.annotations.NonNull;
-import org.lwjgl.PointerBuffer;
-import org.lwjgl.opencl.CL;
-import org.lwjgl.opencl.CL10;
-import org.lwjgl.opencl.CLCapabilities;
-import org.lwjgl.system.MemoryStack;
-
-import java.nio.ByteBuffer;
-import java.nio.IntBuffer;
-import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 public final class PhysicsCommon {
